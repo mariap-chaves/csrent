23baa147843d3762e44af845096475430723ff84
mariap-chaves <maria.chaves24@ucr.ac.cr>
2025-06-26 10:59:10 -0600

    Reservation Fix

--- src/main/java/com/csrent/controller/ReservationController.java
+++ src/main/java/com/csrent/controller/ReservationController.java
@@ -4,6 +4,7 @@ import com.csrent.dto.ReservationDTO;
 import com.csrent.model.Reservation;
 import com.csrent.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,16 +14,25 @@ import java.util.List;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+    @Autowired
     ReservationService reservationService;
-    ReservationDTO reservationDTO;
 
     @GetMapping
-    public ResponseEntity<List<Reservation>> getAll() {
-        return ResponseEntity.ok(reservationService.getAll());
+    public ResponseEntity<?> getAll() {
+        List<?> items = reservationService.getAll();
+        if(items == null || items.isEmpty()){
+            return ResponseEntity.ok("No hay reservaciones registrados");
+        } else {
+            return ResponseEntity.ok(reservationService.getAll());
+        }
     }
 
     @PostMapping
-    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationDTO reservationDTO) {
-        return ResponseEntity.ok(reservationService.add(reservationDTO));
+    public ResponseEntity<?> createReservation(@RequestBody ReservationDTO reservationDTO) {
+        Reservation created = reservationService.add(reservationDTO);
+        if (created == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al crear la reservacion");
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
     }
 }
--- src/main/java/com/csrent/service/ReservationService.java
+++ src/main/java/com/csrent/service/ReservationService.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Service
 public class ReservationService {
 
+@Autowired
 private ReservationRepository reservationRepository;
 private UserService userService;
 private SpaceService spaceService;
